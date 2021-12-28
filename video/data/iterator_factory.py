14bf487ec632bfd1287568660d541602620419a3
Arun George Zachariah <az2z7@mail.umkc.edu>
2021-12-28 17:15:25 -0600

    Initial changes done for re-training on MSR-VTT dataset.

--- video/data/iterator_factory.py
+++ video/data/iterator_factory.py
@@ -234,6 +234,61 @@ def get_hollywood2(data_root='./dataset/Hollywood2',
 
     return (train, val)
 
+def get_msr_vtt(data_root='./dataset/MSR_VTT',
+               clip_length=8,
+               train_interval=2,
+               val_interval=2,
+               mean=[0.485, 0.456, 0.406],
+               std=[0.229, 0.224, 0.225],
+               seed=torch.distributed.get_rank() if torch.distributed._initialized else 0,
+               **kwargs):
+    logging.debug("VideoIter:: clip_length = {}, interval = [train: {}, val: {}], seed = {}".format( \
+                clip_length, train_interval, val_interval, seed))
+
+    normalize = transforms.Normalize(mean=mean, std=std)
+
+    train_sampler = sampler.RandomSampling(num=clip_length,
+                                           interval=train_interval,
+                                           speed=[1.0, 1.0],
+                                           seed=(seed+0))
+    train = VideoIter(video_prefix=os.path.join(data_root, 'raw', 'data'),
+                      txt_list=os.path.join(data_root, 'raw', 'list_cvt', 'train.txt'),
+                      sampler=train_sampler,
+                      force_color=True,
+                      video_transform=transforms.Compose([
+                                         transforms.RandomScale(make_square=True,
+                                                                aspect_ratio=[0.8, 1./0.8],
+                                                                slen=[224, 288]),
+                                         transforms.RandomCrop((224, 224)), # insert a resize if needed
+                                         transforms.RandomHorizontalFlip(),
+                                         transforms.RandomHLS(vars=[15, 35, 25]),
+                                         transforms.ToTensor(),
+                                         normalize,
+                                      ],
+                                      aug_seed=(seed+1)),
+                      name='train',
+                      shuffle_list_seed=(seed+2),
+                      )
+
+    val_sampler   = sampler.SequentialSampling(num=clip_length,
+                                               interval=val_interval,
+                                               fix_cursor=True,
+                                               shuffle=True)
+    val   = VideoIter(video_prefix=os.path.join(data_root, 'raw', 'data'),
+                      txt_list=os.path.join(data_root, 'raw', 'list_cvt', 'test.txt'),
+                      sampler=val_sampler,
+                      force_color=True,
+                      video_transform=transforms.Compose([
+                                         transforms.Resize((256, 256)),
+                                         transforms.CenterCrop((224, 224)),
+                                         transforms.ToTensor(),
+                                         normalize,
+                                      ]),
+                      name='test',
+                      )
+
+    return (train, val)
+
 
 def creat(name, batch_size, num_workers=16, **kwargs):
 
@@ -245,6 +300,8 @@ def creat(name, batch_size, num_workers=16, **kwargs):
         train, val = get_kinetics(**kwargs)
     elif name.upper() == 'HOLLYWOOD2':
         train, val = get_hollywood2(**kwargs)
+    elif name.upper() == 'MSR_VTT':
+        train, val = get_msr_vtt(**kwargs)
     else:
         assert NotImplementedError("iter {} not found".format(name))
 
