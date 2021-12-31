12a3744d5765e86bc1df8e946432b986260f1df3
Arun George Zachariah <az2z7@mail.umkc.edu>
2021-12-31 15:25:03 -0600

    Listing steps

--- video/dataset/SVW/scripts/create_training_lists.py
+++ video/dataset/SVW/scripts/create_training_lists.py
@@ -3,7 +3,8 @@
 
 # To download the dataset: wget https://www.cse.msu.edu/computervision/SVW.zip && unzip SVW.zip
 # To create the data directories: mkdir -p /mydata/CSQ/Hadamard-Matrix-for-hashing/video/dataset/SVW/raw/data && cd /mydata/Videos && mv * /mydata/CSQ/Hadamard-Matrix-for-hashing/video/dataset/SVW/raw/data
-# Remove spaces from files: cd /mydata/CSQ/Hadamard-Matrix-for-hashing/video/dataset/SVW/scripts && bash rename.sh
+# Remove spaces from data files: cd /mydata/CSQ/Hadamard-Matrix-for-hashing/video/dataset/SVW/scripts && bash rename.sh
+# Remove spaces from test and train files: sed -i 's/ - Copy/_-_Copy/g' ../raw/list_cvt/train.txt && sed -i 's/ - Copy/_-_Copy/g' ../raw/list_cvt/test.txt
 
 import pandas as pd
 
