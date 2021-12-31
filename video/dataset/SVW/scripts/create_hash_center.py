3cb1641fd3c5d0f094956d8539d5d74789fec201
Arun George Zachariah <az2z7@mail.umkc.edu>
2021-12-31 12:09:51 -0600

    Initial change done for evaluating CSQ on the SVW dataset

---
+++ video/dataset/SVW/scripts/create_hash_center.py
@@ -0,0 +1,23 @@
+from scipy.linalg import hadamard
+import torch
+import numpy as np
+
+if __name__ == "__main__":
+    num_class = 12
+    d_vals = [16, 32, 64]
+
+    for d in d_vals:
+        ha_d = hadamard(d)
+        ha_2d = np.concatenate((ha_d, -ha_d),0)
+
+        if num_class <= d:
+            hash_targets = torch.from_numpy(ha_d[0:num_class]).float()
+            print('hash centers shape: {}'.format(hash_targets.shape))
+        elif num_class > d:
+            hash_targets = torch.from_numpy(ha_2d[0:num_class]).float()
+            print('hash centers shape: {}'.format(hash_targets.shape))
+
+        file_name = str(d) + '_hollywood2_' + str(num_class) + '_class.pkl'
+        file_dir = '../raw/' + file_name
+        f = open(file_dir, "wb")
+        torch.save(hash_targets, f)
---
+++ video/dataset/SVW/scripts/create_training_directories.py
@@ -0,0 +1,60 @@
+# pandas==1.1.5
+
+import pandas as pd
+import os
+import shutil
+
+ANN_FILE = "SVW.csv"
+DATA_DIR = "/mydata/SVW"
+
+if __name__ == "__main__":
+    df = pd.read_csv(ANN_FILE)
+
+    train_counter = 0
+    test_counter = 0
+
+    train_category_dict = {}
+    test_category_dict = {}
+
+    # Creating a dictionary of category and associated files.
+    for index, row in df.iterrows():
+        if not pd.isnull(row['FileName']):
+            file_name = row['FileName']
+            category = row['Genre']
+
+            if row['Train 1?'] == 0:
+                if category in test_category_dict:
+                    file_lst = test_category_dict[category]
+                    file_lst.append(file_name)
+                    test_category_dict[category] = file
+                else:
+                    test_category_dict[category] = [file]
+            else:
+                if category in train_category_dict:
+                    file_lst = train_category_dict[category]
+                    file_lst.append(file_name)
+                    train_category_dict[category] = file
+                else:
+                    train_category_dict[category] = [file]
+
+    # Creating the data directories
+    for category in train_category_dict:
+        os.makedirs("../raw/data/" + category, exists_ok=True)
+
+    # Creating the list files and moving the video to the data directories
+    with open("../raw/list_cvt/train.txt" + ".txt", 'a+') as f:
+        counter = 0;
+        for index, category in enumerate(train_category_dict):
+            for file_name in train_category_dict[category]:
+                f.write(str(index) + "	" + str(counter) + "	" + category + "/" + file_name + "\n")
+                counter += 1
+                shutil.copy(DATA_DIR + "/" + file_name, "../raw/data/" + category)
+
+    with open("../raw/list_cvt/test.txt" + ".txt", 'a+') as f:
+        counter = 0;
+        for index, category in enumerate(test_category_dict):
+            for files in test_category_dict[category]:
+                f.write(str(index) + "	" + str(counter) + "	" + category + "/" + files + "\n")
+                counter += 1
+                shutil.copy(DATA_DIR + "/" + file_name, "../raw/data/" + category)
+
